8c07df7a73f12e069b60b6957d97f309d47ddb7a
Yeema <e2219755@gmail.com>
2019-03-31 13:53:50 +0800

    access cambridge pages

--- cambridge.dictionary.py
+++ cambridge.dictionary.py
@@ -9,143 +9,117 @@ brace_gx = re.compile(r'\((.+?)\)')
 parts_map = {'U':'N','C':'N','S':'N', 'T':'V' ,'I':'V','L':'V','plural':'N','noun':'N','verb':'V','adjective':'ADJ'}
 eng_set = set('a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'.split(','))
 
-def start_html(url,key):
-
-    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
-
-    source_code = requests.get(url , headers=headers).text
-    soup = BeautifulSoup(source_code, 'html.parser')
-
-    tmp = []
-    header = ""
-    flag = False
-    def_flag = False
-    part = ""
-    partsmap =""
-    for each_text in soup.findAll('div', {'class':'entry-body__el clrd js-share-holder'}):
-        semantic_tag = "NONE"
-        for each in  each_text.findAll('div', {'class':'pos-header'}):
-    #         print(' '.join(each.text.split('\n')[:2]).strip())
-            header = ' '.join(each.text.split('\n')[:2]).strip()
-            if len(each.text.split('\n'))>1:
-                if each.text.split('\n')[1] in parts_map:
-                    part = parts_map[each.text.split('\n')[1]]
-                else:
-                    part = each.text.split('\n')[1]
-            else:
-                if each.text.split(' ')[0] in parts_map:
-                    if each.text.split(' ')[0] in parts_map:
-                        part = parts_map[each.text.split(' ')[0]]
-                    else:
-                        part = each.text.split(' ')[0]
-                else:
-                    part = each.text.split(' ')[0]
-            if regex.search(part):
-                semantic_tag = regex.search(part).group(1).strip()
-                indx = part.find(' [')
-                part = part[:indx].strip()
-                if part in parts_map:
-                    part = parts_map[part]
-
-        for each in each_text.findAll('div', {'class':'sense-block'}):
-            for id,line in enumerate(each.text.split('\n')):
-                line = line.strip()
-                if 'More examples' in line or 'googletag.cmd' in line:
-                    if len(tmp)>1:
-                        if def_flag:
-                            if not tmp[-1].split()[-1][0] in eng_set:
-                                semanticDict[key]['def'][part][partsmap][semantic_tag].append(tmp)
-                                def_flag = False
-                        else:
-                            if semanticDict[key]['ex']:
-                                    semanticDict[key]['ex'].append(tmp)
-                            else:
-                                semanticDict[key]['ex'] = [tmp]
-                        tmp = []
-                    continue
-                if flag:
-                    if brace_gx.search(line):
-                        semantic_tag = brace_gx.search(line).group(1).strip()
-                    flag = False
-                else:
-                    if id == 0 and line == header:
-                        if line and header:
-                            flag = True
-                    else:
-                        if line:
-                            line_split = line.split()[0]
-#                             change here!!!
-                            if line_split[0].isupper() and line_split[-1].isdigit() or line[0]=='›' :
-                                tmp.append(line)
-                                if regex.search(line[2:]):
-                                    partsmap = regex.search(line[2:]).group(1).strip()
-                                else:
-                                    partsmap = "NONE"
-                                def_flag = True
-                            else:
-#                                 print(id,line)
-                                tmp.append(line)
-                                if len(tmp)==2:
-                                    if line.split()[-1][-1].lower() in eng_set:
-                                        if any([r in tmp for r in ['Opposite','Synonym','See also','Compare','See']]):
-                                            tmp = []
-                                        continue
-
-                                    if def_flag:
-                                        semanticDict[key]['def'][part][partsmap][semantic_tag].append(tmp)
-                                        def_flag = False
-                                    else:
-                                        if semanticDict[key]['ex']:
-                                            semanticDict[key]['ex'].append(tmp)
-                                        else:
-                                            semanticDict[key]['ex'] = [tmp]
-                                    tmp = []
-                                elif len(tmp)==3:
-                                    if def_flag:
-                                        if regex.search(tmp[0]) and tmp[0][:len(key)] == key:
-                                            partsmap = regex.search(tmp[0]).group(1).strip()
-                                        semanticDict[key]['def'][part][partsmap][semantic_tag].append(tmp)
-                                        def_flag = False
-                                    else:
-                                        if semanticDict[key]['ex']:
-                                                semanticDict[key]['ex'].append(tmp)
-                                        else:
-                                            semanticDict[key]['ex'] = [tmp]
-                                    tmp = []
-                                elif len(tmp)>3:
-                                    print(tmp)
-                                    if tmp[-1].split()[-1][-1].lower() in eng_set:
-                                        continue
-                                    else:
-                                        if def_flag:
-                                            if regex.search(tmp[0]) and tmp[0][:len(key)] == key:
-                                                partsmap = regex.search(tmp[0]).group(1).strip()
-                                            semanticDict[key]['def'][part][partsmap][semantic_tag].append(tmp)
-                                            def_flag = False
-                                        else:
-                                            if semanticDict[key]['ex']:
-                                                    semanticDict[key]['ex'].append(tmp)
-                                            else:
-                                                semanticDict[key]['ex'] = [tmp]
-                                        tmp = []
-
-            if  def_flag and tmp:
-                semanticDict[key]['def'][part][partsmap][semantic_tag].append(tmp)
-                def_flag = False
-            elif tmp:
-                if semanticDict[key]['ex']:
-                        semanticDict[key]['ex'].append(tmp)
-                else:
-                    semanticDict[key]['ex'] = [tmp]
-                tmp = []
-
 semanticDict = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list)))))
+
+def start(url,head): 
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+    
+    source_code = requests.get(url , headers=headers).text 
+    soup = BeautifulSoup(source_code, 'html.parser') 
+
+    # Text in given web-page is stored under 
+    # the <div> tags with class <entry-content> 
+    for each_text in soup.findAll('div', {'class':'sense-block'}): 
+        clean_sent(each_text.text,head)
+        
+semanticDict = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list)))))
+def build_dict(head,string,skip = 0,semantic_tag = "NONE",definition = "NONE",output_part = "NONE"):
+    special = False
+    example = []
+    output_part = ""
+    part_str = "NONE"
+    ex_id = 0
+#     print(string)
+    for id , s in enumerate(string):
+        cond = s.split()[0]
+
+        if semantic_tag != "NONE" and skip >0 and definition == "NONE":
+            skip -= 1
+            continue
+
+        if skip>0:
+            definition.append(s)
+            ex_id = id+1
+            if output_part:
+                semanticDict[head]['def'][semantic_tag][output_part][part_str].append(definition)
+            else:
+                semanticDict[head]['def'][semantic_tag]['NONE'][part_str].append(definition)
+            skip -= 1
+            continue
+            
+        if cond==head and id+1 < len(string):
+            special = True
+            if s.split()[1].strip() in parts_map:
+                output_part = parts_map[s.split()[1].strip()]
+            elif not output_part:
+                output_part = "NONE"
+            if regex.search(s):
+                part_str = regex.search(s).group(1).strip()
+            else:
+                part_str = "NONE"
+            if brace_gx.search(string[id+1]):
+                semantic_tag = brace_gx.search(string[id+1]).group(1).strip()
+                skip = 1
+            else:
+                semantic_tag = s
+            continue
+        if brace_gx.search(s):  
+            semantic_tag = brace_gx.search(s).group(1).strip()    
+            if semantic_tag.split()[0].isupper():
+                continue
+                
+        if cond[0].isupper() and cond[-1].isdigit() or '›' == cond[0]:
+            if regex.search(s):
+                part_str = regex.search(s).group(1).strip()
+                parts = part_str.split('or')
+                for part in parts:
+                    part = part.strip().split(' ')[0]
+                    if part in parts_map:
+                        output_part = parts_map[part]
+                        break
+                    else:
+                        output_part = part
+#                 s_id = max(s.find(']'),s.find('>'))
+                definition = [s.strip()]
+            else:
+                definition = [s.strip()]
+            skip = 1
+        else:
+            if id ==0:
+                semantic_tag = s
+                print(s)
+            else:
+                if (id-ex_id)%2==0:
+                    if s.split()[-1][0].lower() in eng_set:
+                        example = [s]
+                else:
+                    if len(example)==1:
+                        example.append(s)
+                        if semanticDict[head]['example']:
+                            semanticDict[head]['example'].append(example)
+                        else:
+                            semanticDict[head]['example'] = []
+                            semanticDict[head]['example'].append(example)
+    return semanticDict
+
+def clean_sent(sents,head):
+    string = []
+    example = []
+#     for sent in sent_tokenize(sents.strip()):
+    for sent in sents.strip().split('\n'):
+        for s in sent.split('\n'):
+            s = s.strip()
+            if s:
+                if '更多範例' == s[:4] :
+                    return build_dict(head,string)
+                else:
+                    string.append(s)
+    return build_dict(head,string)
 
 if __name__ == '__main__':
     urls = eval(open('extendURLs.txt').read())
-
-    for r in urls[:10]:
-        start_html(r,r.split('/')[-1])
+    for r in urls:
+        start(r,r.split('/')[-1])
         
     import json
     with open('cambridge.dictionary.json','w') as fd:
